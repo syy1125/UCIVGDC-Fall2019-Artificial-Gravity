c8e5868b62468173e223ca4d28b28e1ca86937dc
Kyle Wong <kylewonga@gmail.com>
2019-10-17 22:42:28 -0700

    Added doors and puzzle element trigger system

---
+++ Assets/Scripts/Interactables/Door.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : PuzzleElement
+{
+    // Start is called before the first frame update
+    private Animator animator;
+    public ActivationType activationType = ActivationType.STANDARD;
+    public PuzzleElement triggeredBy;
+    public float doorSpeed = 1;
+    private float animTime = 0;
+    
+    void Awake(){
+        animator = gameObject.GetComponent<Animator>();
+        if(state==1){
+            animTime = 0;
+        } else {
+            animTime = 1;
+        }
+        triggeredBy.activateEvent += new PuzzleElementEventHandler(onActivate);
+        triggeredBy.deactivateEvent += new PuzzleElementEventHandler(onDeactivate);
+        triggeredBy.toggleEvent += new PuzzleElementEventHandler(onToggle);
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == 0){
+            if(animTime > 0){
+                animTime -= Time.deltaTime*doorSpeed;
+            }
+        } else if(state == 1){
+            if(animTime < 1){
+                animTime += Time.deltaTime*doorSpeed;
+            }
+        }
+        animator.SetFloat("Time",animTime);
+    }
+    public void onActivate(){
+        if(activationType != ActivationType.ONLYDEACTIVATE){
+            state = 1;
+            //animator.SetFloat("DoorSpeed",1f);
+
+        }
+    }
+    public void onDeactivate(){
+        if(activationType != ActivationType.ONLYACTIVATE){
+            state = 0;
+            //animator.SetFloat("DoorSpeed",-1f);
+        }
+    }
+    public void onToggle(){
+        if(state == 0){
+            state = 1;
+            onActivate();
+        } else if(state == 1){
+            state = 0;
+            onDeactivate();
+        }
+    }
+}
---
+++ Assets/Scripts/Interactables/PuzzleElement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ActivationType
+{
+    STANDARD,
+    ONLYACTIVATE,
+    ONLYDEACTIVATE
+}
+public delegate void PuzzleElementEventHandler();
+public class PuzzleElement : MonoBehaviour
+{
+    /*
+    Parent Class for puzzle objects that have a state (door open/closed, button up/down).
+    Puzzle Elements can be triggered by Puzzle Elements or send triggers to Puzzle Elements.
+     */
+    public event PuzzleElementEventHandler activateEvent;
+
+    public event PuzzleElementEventHandler deactivateEvent;
+    public event PuzzleElementEventHandler toggleEvent;
+    public int state = 0; //if an object doesn't/shouldn't have a state, you can just ignore this
+    public void onActivate(){}
+        //called when an object-you-listen-to calls activateOthers
+    public void onDeactivate(){}
+        //called when an object-you-listen-to calls deactivateOthers
+
+    public void onToggle(){}
+    public void activateOthers(){
+        //calls onActivate in all gameObjects listening to this
+        if(activateEvent != null){
+            activateEvent();
+        }
+    }
+    public void deactivateOthers(){
+        //calls onDeactivate in all gameObjects listening to this
+        if(deactivateEvent != null){
+            deactivateEvent();
+        }
+    }
+    public void toggleOthers(){
+        if(toggleEvent != null){
+            toggleEvent();
+        }
+    }
+}
