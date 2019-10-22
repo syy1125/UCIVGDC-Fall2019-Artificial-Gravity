b8085a3686d2828e1448b8818e95e5c5f71b7848
syy1125 <syy1125@hotmail.com>
2019-10-21 18:17:45 -0700

    Disable airborne player auto-orient if near a platform

--- Assets/Scripts/Player/PlayerOrientation.cs
+++ Assets/Scripts/Player/PlayerOrientation.cs
@@ -9,26 +9,30 @@ public class PlayerOrientation : MonoBehaviour
 
 	[Header("Config")]
 	public float RotationRate;
+	/// <summary>
+	/// If there are platforms within this radius, airborne player will not orient to the direction they are looking in
+	/// </summary>
+	public float DisableOrientRadius;
 
 	private float MaxDegreesDelta => RotationRate * Time.fixedDeltaTime;
 
 	private void FixedUpdate()
 	{
 		Transform t = transform;
-		if (!Movement.Grounded)
+		if (Movement.Grounded)
+		{
+			Quaternion startRotation = t.rotation;
+			t.rotation = Quaternion.RotateTowards(
+				startRotation, Quaternion.FromToRotation(-t.up, Gravity.Down) * startRotation, MaxDegreesDelta
+			);
+		}
+		else if (!Physics.CheckSphere(t.position, DisableOrientRadius))
 		{
 			Vector3 oldUp = t.up;
 			t.rotation = Quaternion.RotateTowards(
 				t.rotation, Look.transform.rotation, MaxDegreesDelta
 			);
 			Look.AngleY += Vector3.Angle(oldUp, t.forward) - 90;
-		}
-		else
-		{
-			Quaternion startRotation = t.rotation;
-			t.rotation = Quaternion.RotateTowards(
-				startRotation, Quaternion.FromToRotation(-t.up, Gravity.Down) * startRotation, MaxDegreesDelta
-			);
 		}
 	}
 }
