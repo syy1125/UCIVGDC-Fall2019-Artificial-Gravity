d71d0f9ae76b60eab9329bb65ae206d63c4c5699
syy1125 <syy1125@hotmail.com>
2019-10-22 20:12:42 -0700

    Implement directional gravity control

--- Assets/Scripts/Player/GravityControl.cs
+++ Assets/Scripts/Player/GravityControl.cs
@@ -10,39 +10,59 @@ public class GravityControl : MonoBehaviour
 	public PlayerLook Look;
 	public ArtificialGravity Gravity;
 	public GameObject SurfaceSnapArrow;
-	
+
 	[Header("Config")]
 	public LayerMasks Masks;
 
 	private void Update()
 	{
 		Transform t = Look.transform;
-		Vector3 newDown = t.forward;
-		
-		if (Input.GetAxisRaw("Snap") > 0)
-		{
-			if (Physics.Raycast(t.position, t.forward, out RaycastHit hit, Mathf.Infinity, Masks.GroundMask.value))
-			{
-				SurfaceSnapArrow.SetActive(true);
-				SurfaceSnapArrow.transform.SetPositionAndRotation(
-					hit.point,
-					Quaternion.LookRotation(-hit.normal)
-				);
-				newDown = -hit.normal;
-			}
-			else
-			{
-				return;
-			}
-		}
-		else
-		{
-			SurfaceSnapArrow.SetActive(false);
-		}
 
 		if (Input.GetKeyDown(KeyCode.Mouse0))
 		{
+			Vector3 newDown = t.forward;
+
+			if (Input.GetAxisRaw("Snap") > 0)
+			{
+				if (Physics.Raycast(t.position, t.forward, out RaycastHit hit, Mathf.Infinity, Masks.GroundMask.value))
+				{
+					SurfaceSnapArrow.SetActive(true);
+					SurfaceSnapArrow.transform.SetPositionAndRotation(
+						hit.point,
+						Quaternion.LookRotation(-hit.normal)
+					);
+					newDown = -hit.normal;
+				}
+				else
+				{
+					return;
+				}
+			}
+			else
+			{
+				SurfaceSnapArrow.SetActive(false);
+			}
+
+			Gravity.enabled = true;
 			Gravity.Down = newDown;
+		}
+		else if (Input.GetKeyDown(KeyCode.Mouse1))
+		{
+			var input = new Vector3(
+				Input.GetAxisRaw("Horizontal"),
+				Input.GetAxisRaw("Jump"),
+				Input.GetAxisRaw("Vertical")
+			);
+
+			if (input.magnitude <= float.Epsilon)
+			{
+				Gravity.enabled = false;
+			}
+			else
+			{
+				Gravity.enabled = true;
+				Gravity.Down = t.TransformVector(input);
+			}
 		}
 	}
 }
