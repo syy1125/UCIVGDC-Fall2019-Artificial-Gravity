6b3239ad53a1cc06ddb49a7067b9e57354b651f6
Kyle Wong <kylewonga@gmail.com>
2019-10-24 17:11:48 -0700

    Merged InteractableObject and InteractableButton

--- Assets/Scripts/Interactables/InteractableObject.cs
+++ Assets/Scripts/Interactables/InteractableObject.cs
@@ -7,9 +7,24 @@ public class InteractableObject : Interactable
     /*
         Generic interactable thing that doesn't have any special animation requirements.
      */
-   
+    private Animator MyAnimator;
+    public string InteractAnimation;
+    void Awake()
+    {
+        MyAnimator = gameObject.GetComponent<Animator>();
+    }    
     public override void OnInteract(){
-        DefaultInteract();
+        if(MyAnimator != null && InteractAnimation != ""){
+            if(RepeatTimer > 0)
+                return;
+            DefaultInteract();
+            if(Disabled)
+                return;
+            MyAnimator.Play(InteractAnimation);
+        } else {
+            DefaultInteract();
+        }
+        
     }
     
 }
