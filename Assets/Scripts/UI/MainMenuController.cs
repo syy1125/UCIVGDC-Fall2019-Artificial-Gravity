489fbec3933de95c186c3d5a55d48e9519f449b4
Kyle Wong <kylewonga@gmail.com>
2019-10-28 19:47:06 -0700

    Added basic main menu

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum MenuState {
+        MainMenu,
+        Credits
+    }
+    public string PlaySceneName;
+    public string LevelSelectSceneName;
+
+    public static MenuState State;
+    private bool AllowInput;
+
+    void Awake()
+    {
+        State = MenuState.MainMenu;
+        AllowInput = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButtonPress(){
+        if(!AllowInput)
+            return;
+        AllowInput = false;
+        StartCoroutine(Transition.LoadLevel(PlaySceneName,false));
+    }
+    public void LevelSelectButtonPress(){
+        if(!AllowInput)
+            return;
+        AllowInput = false;
+        StartCoroutine(Transition.LoadLevel(LevelSelectSceneName,false));
+    }
+    public void CreditsButtonPress(){
+        if(!AllowInput)
+            return;
+        State = MenuState.Credits;
+    }
+    public void QuitButtonPress(){
+        if(!AllowInput)
+            return;
+        AllowInput = false;
+        Application.Quit();
+    }
+    
+    
+}
+
---
+++ Assets/Scripts/UI/Transition.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Transition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float TransitionDuration;
+    private Image Overlay;
+    public bool TransitionIntoLevel = false;
+    public static Transition Instance;
+    
+    void Awake()
+    {
+        Instance = this;
+        Overlay = GetComponent<Image>();
+        if(TransitionIntoLevel){
+            StartCoroutine(LoadLevel("",true));
+        }
+    }
+
+   
+    public static IEnumerator LoadLevel(string SceneToLoad, bool Reversed){
+        if(Transition.Instance == null){
+            if(SceneToLoad != "")
+                SceneManager.LoadScene(SceneToLoad);
+            yield break;
+        }
+
+        Image Overlay = Transition.Instance.Overlay;
+        float Duration = Transition.Instance.TransitionDuration;
+        Overlay.enabled = true;
+        float timer = 0;
+        while(timer < Duration){
+            timer += Time.deltaTime;
+            if(!Reversed){
+                //transparent to opaque
+                Color c = Overlay.color;
+                Overlay.color = new Color(c.r,c.g,c.b,timer/Duration);
+            } else {
+                //opaque to transparent
+                Color c = Overlay.color;
+                Overlay.color = new Color(c.r,c.g,c.b,1-(timer/Duration));
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        if(SceneToLoad != ""){
+            SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+}
