33e9046b95dfcb0d5e3796d61594ee00f06fb7c7
syy1125 <syy1125@hotmail.com>
2019-10-28 18:43:01 -0700

    Implement graviton surge

---
+++ Assets/Scripts/Player/GravitonSurge.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class GravitonSurge : MonoBehaviour
+{
+	[Header("References")]
+	public ArtificialGravity Gravity;
+
+	[Header("Config")]
+	public float Multiplier;
+	public float MaxDuration;
+	public float RechargeDelay;
+	public float RechargeRate;
+
+	private float _baseGravity;
+	private float _surgeGravity;
+	public float Charge { get; private set; }
+	private float _lastActive;
+
+	private void Start()
+	{
+		_baseGravity = Gravity.Gravity;
+		_surgeGravity = _baseGravity * Multiplier;
+		Charge = MaxDuration;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.X) && Charge > 0)
+		{
+			Gravity.Gravity = _surgeGravity;
+			Charge -= Time.deltaTime;
+			_lastActive = Time.time;
+		}
+		else
+		{
+			Gravity.Gravity = _baseGravity;
+			if (Time.time - _lastActive > RechargeDelay)
+			{
+				Charge += Time.deltaTime * RechargeRate;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UI/GravitonSurgeChargeBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GravitonSurgeChargeBar : MonoBehaviour
+{
+	[Header("References")]
+	public GravitonSurge Surge;
+	public Image Bar;
+
+	private void Update()
+	{
+		Bar.fillAmount = Surge.Charge / Surge.MaxDuration;
+	}
+}
