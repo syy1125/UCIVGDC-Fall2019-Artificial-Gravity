43eef0cf40a5a2bf5c0d4cd7594990f25573075d
syy1125 <syy1125@hotmail.com>
2019-10-30 18:25:59 -0700

    Create shader lab for experimenting with shaders

---
+++ Assets/Scripts/ShaderLabCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ShaderLabCamera : MonoBehaviour
+{
+	private float _radius;
+	private float _theta;
+	private float _phi;
+
+	private void Start()
+	{
+		Vector3 position = transform.position;
+		_radius = Mathf.Sqrt(position.x * position.x + position.y * position.y + position.z * position.z);
+		_theta = Mathf.Atan2(position.z, position.x);
+		_phi = Mathf.Atan2(Mathf.Sqrt(position.z * position.z + position.x * position.x), position.y);
+	}
+
+	private void LateUpdate()
+	{
+		_radius += Input.GetAxisRaw("Jump") * Time.deltaTime;
+		_theta += Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+		_phi += Input.GetAxisRaw("Vertical") * Time.deltaTime;
+
+		transform.position = new Vector3(
+			_radius * Mathf.Cos(_phi) * Mathf.Cos(_theta),
+			_radius * Mathf.Sin(_phi),
+			_radius * Mathf.Cos(_phi) * Mathf.Sin(_theta)
+		);
+		transform.rotation = Quaternion.LookRotation(-transform.position);
+	}
+}
