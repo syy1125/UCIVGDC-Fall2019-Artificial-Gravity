50805bbaec865ebad7f83985b20d756e2b8a9e95
syy1125 <syy1125@hotmail.com>
2019-11-13 19:45:23 -0800

    Add link capability to ArtificalGravity script

--- Assets/Scripts/ArtificialGravity.cs
+++ Assets/Scripts/ArtificialGravity.cs
@@ -6,16 +6,39 @@ public class ArtificialGravity : MonoBehaviour
 	private Rigidbody Body => _body == null ? _body = GetComponent<Rigidbody>() : _body;
 
 	public float Gravity = 10;
+	// When linked, gravity is effectively the linked script's gravity
+	private ArtificialGravity _link;
+	
 	private Vector3 _down = Vector3.down;
+	
+	
+	// Does not take link into account
 	public Vector3 Down
 	{
 		get => _down;
 		set => _down = value.normalized;
 	}
 
+	// Takes link into account
+	private Vector3 EffectiveDown => _link == null ? Down : _link.Down;
+	private float EffectiveGravity => _link == null ? Gravity : _link.Gravity;
+
+	// `target` is the controller
+	public void ToggleLink(ArtificialGravity target)
+	{
+		if (_link == null)
+		{
+			_link = target;
+		}
+		else
+		{
+			_link = null;
+		}
+	}
+
 	private void FixedUpdate()
 	{
-		Body.AddForce(Down * Gravity, ForceMode.Acceleration);
+		Body.AddForce(EffectiveDown * EffectiveGravity, ForceMode.Acceleration);
 	}
 
 	private void OnDrawGizmosSelected()
