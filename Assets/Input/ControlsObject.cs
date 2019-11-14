6db01223540032aaa97475c0ae832ef682e69270
syy1125 <syy1125@hotmail.com>
2019-11-13 19:18:44 -0800

    Fix memory leak

--- Assets/Input/ControlsObject.cs
+++ Assets/Input/ControlsObject.cs
@@ -9,23 +9,7 @@ public class ControlsObject : ScriptableObject
 
 	private static ControlsWrapper CreateWrapper()
 	{
-		var user = new InputUser();
 		var controls = new ControlsWrapper();
-
-		if (controls.controlSchemes.Count > 0)
-		{
-			foreach (InputDevice device in InputSystem.devices)
-			{
-				if (controls.controlSchemes[0].SupportsDevice(device))
-				{
-					user = InputUser.PerformPairingWithDevice(device, user);
-				}
-			}
-
-			user.ActivateControlScheme(controls.controlSchemes[0]);
-			user.AssociateActionsWithUser(controls);
-		}
-
 		controls.Enable();
 		return controls;
 	}
