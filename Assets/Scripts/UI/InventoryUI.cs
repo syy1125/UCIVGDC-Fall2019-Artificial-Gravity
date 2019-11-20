c81094a7553f430837f78cd8723c04e11cb508b1
syy1125 <syy1125@hotmail.com>
2019-11-20 04:09:47 -0800

    Inventory UI handles null inventory (during player respawn)

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -5,37 +5,46 @@ using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private PlayerInventory Inventory => Player.Instance == null ? null : Player.Instance.Inventory;
-    private Image[] InventorySlots;
-    public Sprite EmptySprite;
-    
-    void Start()
-    {
-        InventorySlots = GetChildren();
-    }
+	// Start is called before the first frame update
+	private PlayerInventory Inventory => Player.Instance == null ? null : Player.Instance.Inventory;
+	private Image[] InventorySlots;
+	public Sprite EmptySprite;
 
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateInventory();
-    }
-    void UpdateInventory(){
-        Sprite sprite = null;
-        for(int i = 0; i < InventorySlots.Length; i++){
-            sprite = Inventory.GetSprite(i);
-            if(sprite == null){
-                InventorySlots[i].sprite = EmptySprite;
-            } else {
-                InventorySlots[i].sprite = Inventory.GetSprite(i);
-            }
-        }
-    }
-    private Image[] GetChildren(){
-        List<Image> imageList = new List<Image>();
-        foreach(Transform child in transform){
-            imageList.Add(child.GetComponent<Image>());
-        }
-        return imageList.ToArray();
-    }
+	void Start()
+	{
+		InventorySlots = GetChildren();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		UpdateInventory();
+	}
+
+	void UpdateInventory()
+	{
+		for (int i = 0; i < InventorySlots.Length; i++)
+		{
+			Sprite sprite = Inventory != null ? Inventory.GetSprite(i) : null;
+			if (sprite == null)
+			{
+				InventorySlots[i].sprite = EmptySprite;
+			}
+			else
+			{
+				InventorySlots[i].sprite = Inventory.GetSprite(i);
+			}
+		}
+	}
+
+	private Image[] GetChildren()
+	{
+		List<Image> imageList = new List<Image>();
+		foreach (Transform child in transform)
+		{
+			imageList.Add(child.GetComponent<Image>());
+		}
+
+		return imageList.ToArray();
+	}
 }
