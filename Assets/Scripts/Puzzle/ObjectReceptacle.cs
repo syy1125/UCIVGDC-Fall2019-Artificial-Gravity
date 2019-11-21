2a35f14ee5f0d3a81cc07bcece37f3bf03689ffb
Kyle Wong <kylewonga@gmail.com>
2019-11-20 19:48:02 -0800

    Added battery+battery receptacle to level 2

---
+++ Assets/Scripts/Puzzle/ObjectReceptacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectReceptacle : PuzzleElement
+{
+    // Start is called before the first frame update
+    public Vector3 ReceptacleOffset;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collider){
+        if(collider.gameObject.GetComponent<LinkableObject>() != null){
+            collider.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            collider.gameObject.transform.position = transform.position + ReceptacleOffset;
+            collider.gameObject.transform.rotation = Quaternion.identity;
+            State = 1;
+            ActivateOthers();
+        }
+    }
+}
