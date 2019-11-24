8d7e7c69f61ee5e0b75d8a3db05150b9ede092ea
syy1125 <syy1125@hotmail.com>
2019-11-23 17:57:08 -0800

    Implement PersistentGameplayScenes

---
+++ Assets/Scripts/ScriptableObjects/PersistentGameplayScenes.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Persistent Gameplay Scenes", fileName = "Persistent Gameplay Scenes")]
+public class PersistentGameplayScenes : ScriptableObject
+{
+	[FormerlySerializedAs("PersistentScenes")] public string[] SceneNames;
+}
