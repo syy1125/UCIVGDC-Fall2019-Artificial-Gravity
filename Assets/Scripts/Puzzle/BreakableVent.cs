e8c5187e3e1c22c7a8edc0e927ddd7e8225d7f8b
Kyle Wong <kylewonga@gmail.com>
2019-11-25 19:11:28 -0800

    Add breakable vents to level 2

---
+++ Assets/Scripts/Puzzle/BreakableVent.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableVent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody _body;
+    public string PhysicsLayer = "Debris";
+    public float ExplosionForce = 500f;
+    public float BreakIfPlayerFasterThan = 20f;
+    private bool _exploded;
+    private float _lastFrameVelocity;
+    private float _currentFrameVelocity;
+    Rigidbody _playerBody;
+
+
+    void Awake()
+    {
+        GetComponent<MeshCollider>().convex = true;
+        _body = GetComponent<Rigidbody>();
+        _body.useGravity = false;
+        _body.isKinematic = true;
+        _exploded = false;
+    }
+    void Start(){
+        _playerBody = Player.Instance.GetComponent<Rigidbody>();
+        _currentFrameVelocity = _playerBody.velocity.magnitude;
+        _lastFrameVelocity = _currentFrameVelocity;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _lastFrameVelocity = _currentFrameVelocity;
+        _currentFrameVelocity = _playerBody.velocity.magnitude;
+
+    }
+
+    public void OnCollisionEnter(Collision collision){
+        if(_exploded){
+            return;
+        }
+        Player player = collision.gameObject.GetComponent<Player>();
+        print(_lastFrameVelocity);
+        if(player != null && _lastFrameVelocity >= BreakIfPlayerFasterThan){
+            player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            transform.localScale *= 0.8f;
+            _body.isKinematic = false;
+            _body.useGravity = true;
+            _body.gameObject.layer = LayerMask.NameToLayer(PhysicsLayer);
+            _body.AddExplosionForce(ExplosionForce, player.transform.position,10f);
+            _exploded = true;
+        }
+    }
+}
