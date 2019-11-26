9de8d343560aafe2cb6e1e929be1b448ab35e777
syy1125 <syy1125@hotmail.com>
2019-11-25 19:47:51 -0800

    Implement downward arrow on HUD

---
+++ Assets/Scripts/UI/DownArrowController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DownArrowController : MonoBehaviour
+{
+	private void Update()
+	{
+		if (Player.Instance != null)
+		{
+			var gravity = Player.Instance.GetComponent<ArtificialGravity>();
+			Transform cameraTransform = Player.Instance.GetComponentInChildren<Camera>().transform;
+			if (gravity != null)
+			{
+				transform.rotation = Quaternion.LookRotation(
+					cameraTransform.InverseTransformVector(gravity.Down)
+				);
+			}
+		}
+	}
+}
