6ad8cbb5ad04530c2d3811c1f93e3193e39bfe59
syy1125 <syy1125@hotmail.com>
2019-11-27 20:10:59 -0800

    Implement music player script

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	private static MusicPlayer _instance;
+	
+	public AudioClip[] Clips;
+
+	private AudioSource _player;
+
+	private void Awake()
+	{
+		if (_instance == null)
+		{
+			_instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else if (_instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private IEnumerator Start()
+	{
+		_player = GetComponent<AudioSource>();
+
+		while (true)
+		{
+			foreach (AudioClip clip in Clips)
+			{
+				_player.clip = clip;
+				_player.Play();
+				yield return new WaitWhile(() => _player.isPlaying);
+			}
+		}
+	}
+	
+	
+}
