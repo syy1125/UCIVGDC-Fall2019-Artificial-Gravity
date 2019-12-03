c5130b97bf1259aa2dfbf04e9fa98baea08b4a7c
Kyle Wong <kylewonga@gmail.com>
2019-12-02 18:58:10 -0800

    Level 2 rocks spin

---
+++ Assets/Scripts/Puzzle/RockSpin.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpin : MonoBehaviour
+{
+    public enum Axis{
+        X,Y,Z,Random
+    }
+    public Axis RotateAxis;
+    public float RotationSpeed;
+    
+    private float Timer;
+    private Vector3 _rotation;
+    void Awake()
+    {
+        
+        if(RotateAxis == Axis.Random){
+            RotateAxis = (Axis)Random.Range(0,3);
+        }
+        switch(RotateAxis){
+            case Axis.X:
+                _rotation = new Vector3(RotationSpeed,0,0);
+                break;
+            case Axis.Y:
+                _rotation = new Vector3(0,RotationSpeed,0);
+                break;
+            case Axis.Z:
+                _rotation = new Vector3(0,0,RotationSpeed);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(_rotation*Time.deltaTime);
+    }
+}
